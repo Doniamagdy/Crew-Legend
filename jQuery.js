bf4e420c793be77163371f70d2cd7fe9ab33e961
Doniamagdy <95994116+Doniamagdy@users.noreply.github.com>
2024-06-24 16:54:09 +0300

    Add files via upload

---
+++ jQuery.js
@@ -0,0 +1,146 @@
+$(".brown-btn").click(function(){
+    $(".row").removeClass("hidden");
+  });
+
+  $(".brown-btn2").click(function(){
+    $("#card1").removeClass("hidden");
+  });
+
+
+  $(".brown-btn3").click(function(){
+    $("#card2").removeClass("hidden");
+  });
+
+
+  $(".brown-btn4").click(function(){
+    $("#card3").removeClass("hidden");
+  });
+
+
+  $(".brown-btnx").click(function(){
+    $("#box1").removeClass("hidden");
+  });
+
+
+  $(".brown-btnx2").click(function(){
+    $("#box2").removeClass("hidden");
+  });
+
+
+  $(".brown-btny1").click(function(){
+    $("#box4").removeClass("hidden");
+  });
+
+  $(".brown-btny2").click(function(){
+    $("#box5").removeClass("hidden");
+  });
+
+
+  $(".brown-btnz").click(function(){
+    $("#box6").removeClass("hidden");
+  });
+
+  $(".brown-btnz1").click(function(){
+    $("#box7").removeClass("hidden");
+  });
+
+  $(".brown-btnz2").click(function(){
+    $("#box8").removeClass("hidden");
+  });
+
+  $(".brown-btnz3").click(function(){
+    $("#box9").removeClass("hidden");
+  });
+
+  $(".up").click(function(){
+    $("#down").removeClass("hidden");
+  });
+
+  $(".up2").click(function(){
+    $("#down2").removeClass("hidden");
+  });
+
+  $(".up3").click(function(){
+    $("#down3").removeClass("hidden");
+  });
+
+  $(".up4").click(function(){
+    $("#down4").removeClass("hidden");
+  });
+
+  $(".up5").click(function(){
+    $("#down5").removeClass("hidden");
+  });
+
+  $(".up6").click(function(){
+    $("#down6").removeClass("hidden");
+  });
+
+  $(".up7").click(function(){
+    $("#down7").removeClass("hidden");
+  });
+
+  $(".up8").click(function(){
+    $("#down8").removeClass("hidden");
+  });
+
+
+  $(".brown-btnD1").click(function(){
+    $("#cardx1").removeClass("hidden");
+  });
+
+
+  
+  $(".brown-btnD2").click(function(){
+    $("#cardx2").removeClass("hidden");
+  });
+
+
+  $(".brown-btnM1").click(function(){
+    $("#boxM1").removeClass("hidden");
+  });
+
+
+  $(".upM1").click(function(){
+    $("#downM").removeClass("hidden");
+  });
+
+
+    $(".brown-btnM2").click(function(){
+    $("#boxM2").removeClass("hidden");
+  });
+
+
+  $(".upM2").click(function(){
+    $("#downM2").removeClass("hidden");
+  });
+
+
+  $(".brown-btnM3").click(function(){
+    $("#boxM3").removeClass("hidden");
+  });
+
+
+  $(".upM3").click(function(){
+    $("#downM3").removeClass("hidden");
+  });
+
+
+  $(".brown-btnM4").click(function(){
+    $("#boxM4").removeClass("hidden");
+  });
+
+
+  $(".upM4").click(function(){
+    $("#downM4").removeClass("hidden");
+  });
+
+
+  $(".brown-btnM5").click(function(){
+    $("#boxM5").removeClass("hidden");
+  });
+
+
+  $(".upM5").click(function(){
+    $("#downM5").removeClass("hidden");
+  });
